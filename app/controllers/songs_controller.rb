4c5cba72f1f26190a7653a461044515d41bfe196
Prelang Builder <builder@prelang.com>
2014-07-04 03:09:20 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :edit, :update, :destroy]
+  before_action :set_song, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /songs
   # GET /songs.json
